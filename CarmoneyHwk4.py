6eaaeb4e98af141ad95bce63054063b71e8a3852
carmoca <carmoca@gmail.com>
2015-03-01 22:07:42 -0500

    Updated file to contain a distance map
    Randomly grabbed from 'teh internets')

--- CarmoneyHwk4.py
+++ CarmoneyHwk4.py
@@ -4,18 +4,33 @@
 #Professor:  Bruno A.
 #
 #Comments from various runs are below the sourcecode
+#Assignment #4
 
-# coding=utf-8
-from simpleai.search import SearchProblem, astar
+from simpleai.search import SearchProblem, hill_climbing_random_restarts
 import sys
 import pdb
+import random
 import time
 
-class WaterJugsProblem(SearchProblem):
+class TspProblem(SearchProblem):
     #Jugs problem
 
     def __init__(self,j1,j2):
-        super(WaterJugsProblem, self).__init__(initial_state=(0, 0))
+        super(TspProblem, self).__init__(initial_state=(0, 0))
+        self.distances = \
+            [[  0,5472,5092,5393,5416,4584,4905,3852,4477,5261,4843,5930], \
+            [5472,   0,1315, 927,1505, 944,1157,1945,7225,1127, 687, 459], \
+            [5092,1315,   0,2166,2724,1572, 294,1241,6029, 236,1489,1542], \
+            [5393, 927,2166,  0 , 578, 973,1947,2422,7793,2010, 819,1096], \
+            [5416,1505,2724, 578,   0,1367,2491,2839,8172,2579,1295,1636], \
+            [4584, 944,1572, 973,1367,   0,1290,1474,6832,1511, 263,1387], \
+            [4905,1157, 294,1947,2491,1290,   0,1064,6094, 362,1228,1458], \
+            [3852,1945,1241,2422,2839,1474,1064,   0,5371,1412,1605,2356], \
+            [4477,7225,6029,7793,8172,6832,6094,5371,   0,6263,6977,7551], \
+            [5261,1127, 236,2010,2579,1511, 362,1412,6263,   0,1392,1319], \
+            [4843, 687,1489, 819,1295, 263,1228,1605,6977,1392,   0,1125], \
+            [5930, 459,1542,1096,1636,1387,1458,2356,7551,1319,1125,   0]]
+
         self.jug1=j1
         self.jug2=j2
 
@@ -105,6 +120,14 @@ class WaterJugsProblem(SearchProblem):
     #Result of applying an action to a state.
     def result(self, s, a):
         return a[1]
+
+    def value(self, s, v):
+        return s[0]
+
+    def generate_random_restart(self, s, v):
+        return s[0]
+
+    def _tour_length(self, s)
 
     def is_goal(self, state):
         return state == (2, 0)
