0423d73f6bed7c36f0e5f08b1a6f036f8040849d
carmonc <carmonc@sunyit.edu>
2015-03-05 08:55:47 -0500

    Successfully finds a route. I think...

--- CarmoneyHwk4.py
+++ CarmoneyHwk4.py
@@ -1,5 +1,5 @@
 #Author:     Carl Carmoney
-#Date:       Feb 25, 2015
+#Date:       Mar 04, 2015
 #Subject:    CS495-35W Artificial Intelligence
 #Professor:  Bruno A.
 #
@@ -20,7 +20,9 @@ class TspProblem(SearchProblem):
         self.num_cities=len(self.distances)
         self.rows=len(self.distances[0])
         self.tour = []
+        #self.route = initial[1:]
         self.route = initial
+        print self.route
         self.hops = len(self.route)
         self._max = self.calc_max()
 
@@ -95,12 +97,11 @@ class TspProblem(SearchProblem):
 
         return ( endpoints_same and result ) 
 
-    # FIXME! What's going on here?
     def result(self, s, a):
         '''
         Result of applying an action to a state.
         '''
-        return a[0]
+        return a[1]
 
     def value(self, s):
         ''' 
@@ -113,8 +114,7 @@ class TspProblem(SearchProblem):
         neworder = []
         alist = self.route[:]
         neworder.append(alist.pop(0))
-        last = alist.pop(len(alist)-1)
-        
+        last = alist.pop(len(alist)-1)        
         for i in range(0,len(alist)):
             neworder.append(alist.pop(random.randint(0,len(alist)-1)))
         neworder.append(last)
@@ -128,12 +128,13 @@ class TspProblem(SearchProblem):
         #HINT: Do not use a foreach loop.
         
         tourlen = 0
+        lhs_idx = 0
+        rhs_idx = 0
         hops = len(s)
-        for item in range(0,hops-1):
+        for item in range(0,hops-2):            
             lhs_idx = s[item]
             rhs_idx = s[item+1]
-            #tourlen += self.distances[int(lhs_idx)][int(rhs_idx)]
-            tourlen = tourlen + self.distances[int(lhs_idx)][int(rhs_idx)]
+            tourlen = tourlen + self.distances[lhs_idx][rhs_idx]
         return tourlen 
 
     #FIXME!
@@ -143,27 +144,42 @@ class TspProblem(SearchProblem):
     def heuristic(self, state):
         return 1
 
-#if len(sys.argv) != 3:
-#    print('This program takes arguments - try using some next time!')
-#else:
-starttime = time.time()
-       #   00   01   02   03   04   05   06   07   08   09   10   11
-matrix= \
-        [[  0,5472,5092,5393,5416,4584,4905,3852,4477,5261,4843,5930], \
-        [5472,   0,1315, 927,1505, 944,1157,1945,7225,1127, 687, 459], \
-        [5092,1315,   0,2166,2724,1572, 294,1241,6029, 236,1489,1542], \
-        [5393, 927,2166,   0, 578, 973,1947,2422,7793,2010, 819,1096], \
-        [5416,1505,2724, 578,   0,1367,2491,2839,8172,2579,1295,1636], \
-        [4584, 944,1572, 973,1367,   0,1290,1474,6832,1511, 263,1387], \
-        [4905,1157, 294,1947,2491,1290,   0,1064,6094, 362,1228,1458], \
-        [3852,1945,1241,2422,2839,1474,1064,   0,5371,1412,1605,2356], \
-        [4477,7225,6029,7793,8172,6832,6094,5371,   0,6263,6977,7551], \
-        [5261,1127, 236,2010,2579,1511, 362,1412,6263,   0,1392,1319], \
-        [4843, 687,1489, 819,1295, 263,1228,1605,6977,1392,   0,1125], \
-        [5930, 459,1542,1096,1636,1387,1458,2356,7551,1319,1125,   0]]
-problem = TspProblem(matrix, [0, 4, 3, 2, 1, 8, 7, 6, 5, 11, 10, 9, 0])
-#problem = TspProblem(matrix , sys.argv[2:])
-result = hill_climbing_random_restarts(problem, restarts_limit=200)
-endtime = time.time()
-print result.path()
-print('It took ' + str(endtime - starttime) + ' to compute this answer.')
+def usage():    
+    print 'This program takes arguments - try using some!'
+    print '\n\nex: python CarmoneyHwk4.py 0 1 2 3 4 5 6 7 0'
+    print '\n    The route must begin and end with the same value\n    such as \'0\' in the example.'
+    print '\n    Minimum amount of routes is 4'
+    print '    Maximum amount of routes is 12'
+    
+        
+if 4 < len(sys.argv) < 15:
+           #   00   01   02   03   04   05   06   07   08   09   10   11
+    matrix= \
+            [[  0,5472,5092,5393,5416,4584,4905,3852,4477,5261,4843,5930], \
+            [5472,   0,1315, 927,1505, 944,1157,1945,7225,1127, 687, 459], \
+            [5092,1315,   0,2166,2724,1572, 294,1241,6029, 236,1489,1542], \
+            [5393, 927,2166,   0, 578, 973,1947,2422,7793,2010, 819,1096], \
+            [5416,1505,2724, 578,   0,1367,2491,2839,8172,2579,1295,1636], \
+            [4584, 944,1572, 973,1367,   0,1290,1474,6832,1511, 263,1387], \
+            [4905,1157, 294,1947,2491,1290,   0,1064,6094, 362,1228,1458], \
+            [3852,1945,1241,2422,2839,1474,1064,   0,5371,1412,1605,2356], \
+            [4477,7225,6029,7793,8172,6832,6094,5371,   0,6263,6977,7551], \
+            [5261,1127, 236,2010,2579,1511, 362,1412,6263,   0,1392,1319], \
+            [4843, 687,1489, 819,1295, 263,1228,1605,6977,1392,   0,1125], \
+            [5930, 459,1542,1096,1636,1387,1458,2356,7551,1319,1125,   0]]
+
+    route = []
+    for i in sys.argv[1:]:
+        route.append(int(i))
+    if route[0] == route[-1]:
+        starttime = time.time()
+        problem = TspProblem(matrix , route)
+        result = hill_climbing_random_restarts(problem, restarts_limit=200)
+        endtime = time.time()
+        for i in result.path():
+            print i
+        print('It took ' + str(endtime - starttime) + ' to compute this answer.')
+    else:
+        usage()
+else:
+    usage()
